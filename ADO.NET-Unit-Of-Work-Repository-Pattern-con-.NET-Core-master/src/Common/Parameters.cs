237cf88d94d3adc7c385a1cd83fd9788bd293408
Gaviles1080 <102992568+Gaviles1080@users.noreply.github.com>
2022-04-06 16:46:59 -0700

    Add files via upload

---
+++ ADO.NET-Unit-Of-Work-Repository-Pattern-con-.NET-Core-master/src/Common/Parameters.cs
@@ -0,0 +1,8 @@
+﻿namespace Common
+{
+    public class Parameters
+    {
+        public const string ConnectionString = "Server=DESKTOP-M9KD390\\SQLEXPRESS;Initial Catalog=KodotiSells;Integrated Security=true;";
+        public const decimal IvaRate = 0.18m;
+    }
+}
